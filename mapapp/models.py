e0f78589de476ce15c4eaeef6318a3f0d6f76372
s-zhaojo <56804773+s-zhaojo@users.noreply.github.com>
2025-12-24 15:41:49 -0800

    Delete mapapp directory

--- mapapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Reservoir(models.Model):
-    name = models.CharField(max_length=200, unique=True)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-
-    def __str__(self):
-        return self.name
--- mapapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import map_view, reservoir_search
-
-urlpatterns = [
-    path('', map_view, name='map'),
-    path('api/reservoirs/', reservoir_search, name='reservoir-search'),
-]
