0be25cb31d2cfa287f5dea956d8fa7c1c8a34577
s-zhaojo <56804773+s-zhaojo@users.noreply.github.com>
2025-12-24 16:08:44 -0800

    Delete worldmap directory

--- worldmap/mapapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Reservoir
-
-@admin.register(Reservoir)
-class ReservoirAdmin(admin.ModelAdmin):
-    list_display = ("name", "latitude", "longitude")
-    search_fields = ("name",)
